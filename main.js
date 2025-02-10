efa829be1b9aefeae9e2b1feacb47c5491963735
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-10 15:26:44 +0100

    edit final price function and main.js refactoring

--- main.js
+++ main.js
@@ -1,38 +1,33 @@
-// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// va applicato uno sconto del 20% per i minorenni
-// va applicato uno sconto del 40% per gli over 65.
-// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
-// Bonus:
-// Svolgi in bonus in una cartella /bonus nella repo.
-// pari/dispari: completare il live iniziato in classe con Leonardo e risolvere il bug lasciato. L'utente vince solo con pari e se sceglie dispari?  Tips: é necessario l'operatore logico AND usato nella condizione? forse sarebbe meglio separare le due condizioni e verificare prima se il risultato é pari e salvare da qualche parte questa informazione ma occhio all'ambito di visibilitá delle variabili.
-// quanto manca:  Stabilire l'ora attuale e attraverso una operazione matematica stabilire i minuti mancanti allo scoccare della prossima ora.  Oltre a Math ci sono anche altre funzionalitá di javascript (built-in) che possaimo usare senza installazioni come ad esempio le funzioni dell'oggetto Date.
+// Chiedo all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
 
+const userKm = parseInt(prompt('Quanti chilometri vuoi percorrere?'))
 
-
-// Chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-
-// userKm = parseInt(prompt('Quanti chilometri vuoi percorrere?'))
-
-// userAge = parseInt(prompt('Quanti anni hai?'))
-
-userKm = 50
-
-userAge = 20
+const userAge = parseInt(prompt('Quanti anni hai?'))
 
 console.log(userKm);
 
 console.log(userAge);
 
-kmPrice = (userKm * 0.21) 
+// Calcolo il prezzo per chilometro 
+
+const kmPrice = (userKm * 0.21)
+
+// Applico lo sconto in base all'età dell'utente
+
+let agePrice = 0
 
 if (userAge<18) {
-    agePrice = kmPrice * 20 / 100
+    agePrice = kmPrice - (kmPrice * 20 / 100)
 } else if (userAge >= 65) {
-    agePrice = kmPrice * 40 / 100
+    agePrice = kmPrice - (kmPrice * 40 / 100)
 } else {
     agePrice = kmPrice
 }
 
-console.log(agePrice);
+// Applico al risultato il formato del prezzo con due decimali
+
+agePrice = agePrice.toFixed(2)
+
+// Stampo a schermo il prezzo del biglietto per l'utente
+
+alert(`Il tuo biglietto costa ${agePrice} €`)
